ed3e84541c6d677a399207aec32c96249fca774c
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-06-29 13:50:02 -0700

    - TODO:     - Think about the next TODO     - We need to see if:         1) use useContext to keep track of solves         2) pass the state as a prop
    - Notes & Summary:
        - Finished the title component

--- src/components/MainDisplay/AppTitle/AppTitle.jsx
+++ src/components/MainDisplay/AppTitle/AppTitle.jsx
@@ -1,10 +1,15 @@
-function AppTitle() {
+import './AppTitle.css'
 
-    return (
-        <>
-            <h1>APP TITLE</h1>
-        </>
-    )
+function AppTitle() {
+  const title = "ON-TRACK";
+  const titleArr = [...title];
+  return (
+    <div className="title-wrapper">
+      {titleArr.map((char, index) => (
+        <h1 key={index} className="letter">{char}</h1>
+      ))}
+    </div>
+  );
 }
 
-export default AppTitle
+export default AppTitle;
